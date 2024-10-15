b3fafa3ca3bae37159e438638b2e2f25ad10e429
jp_vo <jp_vogas@hotmail.com>
2024-10-15 04:57:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const numParticles = 100;
+const minSize = 5;
+const maxSize = 15;
+
+function createParticles() {
+    for (let i = 0; i < numParticles; i++) {
+        const particle = document.createElement('div');
+        const size = Math.random() * (maxSize - minSize) + minSize;
+        particle.className = 'particle';
+        particle.style.width = `${size}px`;
+        particle.style.height = `${size}px`;
+        particle.style.top = `${Math.random() * window.innerHeight}px`;
+        particle.style.left = `${Math.random() * window.innerWidth}px`;
+
+        const animationDelay = Math.random() * 5; 
+        particle.style.animationDelay = `${animationDelay}s`;
+        document.body.appendChild(particle);
+    }
+}
+createParticles();
